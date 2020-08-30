64cd27fae328d42300f523ad1bfd9909efd521e2
kalpajpise <kalpaj12@gmail.com>
2020-08-30 22:34:25 +0530

    Assignment 12

--- Assignment 3/index.js
+++ Assignment 3/index.js
@@ -1,24 +1,21 @@
-var header = $('#navbar_Nav');
+var header = $("#navbar_Nav");
 var sticky = header.offset().top;
 
-$(window).on('scroll', function () {
-  if($(window).scrollTop() > sticky) {
-    header.addClass('sticky');
-  }
-
-  else{
-    header.removeClass('sticky');
-  }
+$(window).on("scroll", function () {
+	if ($(window).scrollTop() > sticky) {
+		header.addClass("sticky");
+	} else {
+		header.removeClass("sticky");
+	}
 });
 
-
-$('a').click(function () {
-  $('.selectedLink').removeClass('selectedLink');
-  $(this).addClass('selectedLink');
+$("a").click(function () {
+	$(".selectedLink").removeClass("selectedLink");
+	$(this).addClass("selectedLink");
 });
 
-$('.buy_now_btn, .submit_btn, .sign_up').click(function () {
-  $('.buttonClicked').removeClass('buttonClicked');
-  $(this).addClass('buttonClicked');
-  return false;
+$(".buy_now_btn, .submit_btn, .sign_up").click(function () {
+	$(".buttonClicked").removeClass("buttonClicked");
+	$(this).addClass("buttonClicked");
+	return false;
 });
